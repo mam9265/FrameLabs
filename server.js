732685623e28de4cfbbcfd90459416987cd5f0f3
mam9265 <mam9265@psu.edu>
2025-09-25 19:10:13 -0400

    FrameLabs Server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const FrameLabs = express();
+FrameLabs.use(express.json());
+
+//Array that acts as a "Database"
+let community = [];
+
+//Get Endpoint
+FrameLabs.get('/api/system/community', (req, res) => {
+    //Return all the community guides
+})
+
+const PORT = 3000;
+FrameLabs.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
