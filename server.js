91e79df678f10d80f5ff393df40f7458fcb7abb6
Darkar00 <jacob22032@gmail.com>
2025-09-26 13:02:07 -0400

    Jacob's Commit

--- server.js
+++ server.js
@@ -11,5 +11,15 @@ FrameLabs.get('/api/system/community', (req, res) => {
     res.json(community);
 })
 
+//Delete Community Item
+FrameLabs.delete('/api/system/community', (req, res) => {
+    //Delete community item
+    const deleteIndex = community.findIndex(t => t.id === parseInt(req.params.id));
+    if (deleteIndex === -1) {return res.status(404).send('Given ID was not found.');}
+    community.splice(deleteIndex, 1);
+    res.status(204).send();     //204 - No content for successful deletion
+})
+
+//Port the system runs on :)
 const PORT = 3000;
 FrameLabs.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
