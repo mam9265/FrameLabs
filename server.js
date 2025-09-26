9e6ce136679253289fa68df5fd885bc02cb61784
Isaac Bruner <157771971+Corax432@users.noreply.github.com>
2025-09-26 14:00:30 -0400

    On branch Isaac  Changes to be committed: 	modified:   server.js

--- server.js
+++ server.js
@@ -1,25 +1,20 @@
 const express = require('express');
-const FrameLabs = express();
-FrameLabs.use(express.json());
+const app = express();
+app.use(express.json());
 
-//Array that acts as a "Database"
 let community = [];
 
-//Get Endpoint
-FrameLabs.get('/api/system/community', (req, res) => {
-    //Return all the community guides
-    res.json(community);
+app.put('/api/system/community/:id', (req, res) => {
+    const guide = community.find(t => t.id === parseInt(req.params.id)); 
+    if (!guide){
+        return res.status(404).send(`Guide not found.`)
+    }
+    guide.text = req.body.text;
+    res.json (guide);
+    res.status(204).send(`Guide updated.`);
 })
 
-//Delete Community Item
-FrameLabs.delete('/api/system/community', (req, res) => {
-    //Delete community item
-    const deleteIndex = community.findIndex(t => t.id === parseInt(req.params.id));
-    if (deleteIndex === -1) {return res.status(404).send('Given ID was not found.');}
-    community.splice(deleteIndex, 1);
-    res.status(204).send();     //204 - No content for successful deletion
-})
 
-//Port the system runs on :)
 const PORT = 3000;
-FrameLabs.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+
+app.listen(PORT, () => console.log (`Server running on port ${PORT}`));
