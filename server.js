768eee2f5a208cc3c0f2be34160b1d1660625e5d
Jacob Riggs <jacob22032@gmail.com>
2025-10-02 11:08:32 -0400

    Did I do this right?

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ FrameLabs.use(express.json());
 
 //Array that acts as a "Database"
 let community = [];
+let system = [];
+let user = [];
 
 //Get Endpoint
 FrameLabs.get('/api/system/community', (req, res) => {
@@ -17,6 +19,24 @@ FrameLabs.delete('/api/system/community', (req, res) => {
     const deleteIndex = community.findIndex(t => t.id === parseInt(req.params.id));
     if (deleteIndex === -1) {return res.status(404).send('Given ID was not found.');}
     community.splice(deleteIndex, 1);
+    res.status(204).send();     //204 - No content for successful deletion
+})
+
+//Delete System Item
+FrameLabs.delete('/api/system/system', (req, res) => {
+    //Delete system item
+    const deleteIndex = system.findIndex(t => t.id === parseInt(req.params.id));
+    if (deleteIndex === -1) {return res.status(404).send('Given ID was not found.');}
+    system.splice(deleteIndex, 1);
+    res.status(204).send();     //204 - No content for successful deletion
+})
+
+//Delete User Item
+FrameLabs.delete('/api/system/user', (req, res) => {
+    //Delete user item
+    const deleteIndex = user.findIndex(t => t.id === parseInt(req.params.id));
+    if (deleteIndex === -1) {return res.status(404).send('Given ID was not found.');}
+    user.splice(deleteIndex, 1);
     res.status(204).send();     //204 - No content for successful deletion
 })
 
