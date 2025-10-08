224573adf3bf9efbd51a006dd13456f17d2f583a
Jacob Riggs <jacob22032@gmail.com>
2025-10-08 13:11:42 -0400

    Frontend update?

--- frontEnd.js
+++ frontEnd.js
@@ -1,1 +1,23 @@
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 300;
 
+function loadHTML() {
+    fetch('homePage.html')
+        .then(response => response.text())
+        //.then(text => document.getElementById('homePage').innerHTML = text);
+  }
+
+const server = http.createServer((req, res) => {
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    //res.end('');
+
+    loadHTML();
+
+    
+
+});
+
+server.listen(port, hostname, () => {console.log('Server running at http://'+ hostname + ':' + port + '/');});
