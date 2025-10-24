97aa457b6d84cddd6282e73ec2484ae8f70e123e
Jacob Riggs <jacob22032@gmail.com>
2025-10-24 12:30:18 -0400

    Update frontEnd.js

--- frontEnd.js
+++ frontEnd.js
@@ -14,4 +14,4 @@ const server = http.createServer((req, res) => {
 
 });
 
-server.listen(port, hostname, () => {console.log('Server running at http://'+ hostname + ':' + port + '/');});
+//server.listen(port, hostname, () => {console.log('Server running at http://'+ hostname + ':' + port + '/');});
